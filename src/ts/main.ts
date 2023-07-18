2210294336a1409c765a11dc1a61f5c28d944dbc
Adrian Maj <adrianmaj1122@gmail.com>
2023-07-18 17:07:17 +0200

    Added circle effect, bug fixes

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -4,12 +4,13 @@ const rulesModal = document.querySelector('.rules-modal')
 const closeModalBtn = document.querySelector('.rules-modal__close-btn')
 
 //GAME
-const gameSection = document.querySelector('.game')
+const gameSection: HTMLElement = document.querySelector('.game')!
 const gameBackground: HTMLElement = document.querySelector('.game__background')!
 const gameText = document.querySelectorAll('.game__text')
 const gameHouse: HTMLElement = document.querySelector('.game__house')!
 let selectedIcon: HTMLElement
 const gameIcons = document.querySelectorAll('.game__icon')
+const gameContainer: HTMLElement = document.querySelector('.game__container')!
 //RESULT
 const resultBox: HTMLElement = document.querySelector('.result')!
 const resultBtn: HTMLElement = document.querySelector('.result__play-again-btn')!
@@ -32,66 +33,73 @@ const closeModal = () => {
 	rulesModal?.classList.remove('active')
 }
 //GAME
-gameIcons.forEach(icon => {
-	icon.addEventListener('click', e => {
-		selectedIcon = e.target as any
-		selectedIcon.classList.add('selected')
-		gameIcons.forEach(icon => {
-			if (!icon.classList.contains('selected')) {
-				const iconElement = icon as HTMLElement
-				iconElement.style.display = 'none'
-				gameBackground.style.display = 'none'
-				gameText.forEach(text => {
-					if (text.parentElement?.style.display != 'none') {
-						const textElement = text as HTMLElement
-						textElement.style.display = 'block'
-						gameHouse.style.display = 'flex'
-					}
-				})
-			}
-		})
-		setTimeout(() => {
-			gameHouse.classList.remove('game__house')
-			gameHouse.classList.add(randomHouse())
-			const playerNumber: number = Number(selectedIcon.getAttribute('data-id'))
-			resultBox.classList.add('visible')
-			if (
-				(houseNumber == 0 && playerNumber == 0) ||
-				(houseNumber == 1 && playerNumber == 1) ||
-				(houseNumber == 2 && playerNumber == 2)
-			) {
-				resultText.textContent = 'draw'
-			} else if (
-				(houseNumber == 0 && playerNumber == 2) ||
-				(houseNumber == 1 && playerNumber == 0) ||
-				(houseNumber == 2 && playerNumber == 1)
-			) {
-				resultText.textContent = 'you lose'
-				if (score > 0) {
-					score--
+const handleGame = (e: Event) => {
+	selectedIcon = e.target as any
+	selectedIcon.classList.add('selected')
+	selectedIcon.removeEventListener('click', handleGame)
+	selectedIcon.style.margin = '0 auto'
+	gameHouse.style.margin = '0 auto'
+	gameIcons.forEach(icon => {
+		if (!icon.classList.contains('selected')) {
+			const iconElement = icon as HTMLElement
+			iconElement.style.display = 'none'
+			gameBackground.style.display = 'none'
+			gameText.forEach(text => {
+				if (text.parentElement?.style.display != 'none') {
+					const textElement = text as HTMLElement
+					textElement.style.display = 'block'
+					gameHouse.style.display = 'flex'
+					gameSection.style.display = 'flex'
 				}
-				sessionStorage.setItem('score', `${score}`)
-			} else if (
-				(houseNumber == 0 && playerNumber == 1) ||
-				(houseNumber == 1 && playerNumber == 2) ||
-				(houseNumber == 2 && playerNumber == 0)
-			) {
-				resultText.textContent = 'you win'
-				if (score <= 99) {
-					score++
-				}
-				sessionStorage.setItem('score', `${score}`)
-			}
-			// 0 - paper
-			// 1 - scissors
-			// 2 - rock
-		}, 1000)
+			})
+		}
 	})
-})
+	setTimeout(() => {
+		gameHouse.classList.remove('game__house')
+		gameHouse.classList.add(randomHouse())
+		const playerNumber: number = Number(selectedIcon.getAttribute('data-id'))
+		resultBox.classList.add('visible')
+		if (
+			(houseNumber == 0 && playerNumber == 0) ||
+			(houseNumber == 1 && playerNumber == 1) ||
+			(houseNumber == 2 && playerNumber == 2)
+		) {
+			resultText.textContent = 'draw'
+		} else if (
+			(houseNumber == 0 && playerNumber == 2) ||
+			(houseNumber == 1 && playerNumber == 0) ||
+			(houseNumber == 2 && playerNumber == 1)
+		) {
+			resultText.textContent = 'you lose'
+			if (score > 0) {
+				score--
+			}
+			sessionStorage.setItem('score', `${score}`)
+			const circle = document.createElement('div')
+			circle.classList.add('circle-effect')
+			gameHouse.appendChild(circle)
+		} else if (
+			(houseNumber == 0 && playerNumber == 1) ||
+			(houseNumber == 1 && playerNumber == 2) ||
+			(houseNumber == 2 && playerNumber == 0)
+		) {
+			resultText.textContent = 'you win'
+			if (score <= 99) {
+				score++
+			}
+			sessionStorage.setItem('score', `${score}`)
+			const circle = document.createElement('div')
+			circle.classList.add('circle-effect')
+			selectedIcon.appendChild(circle)
+		}
+		// 0 - paper
+		// 1 - scissors
+		// 2 - rock
+	}, 1000)
+}
 const refreshPage = () => {
 	location.reload()
 }
-
 const randomHouse = () => {
 	const number = Math.floor(Math.random() * 3)
 	const selectedSign = gameIcons[number]
@@ -101,5 +109,9 @@ const randomHouse = () => {
 //RULES
 rulesBtn?.addEventListener('click', showModal)
 closeModalBtn?.addEventListener('click', closeModal)
+//GAME
+gameIcons.forEach(icon => {
+	icon.addEventListener('click', handleGame)
+})
 //RESULT
 resultBtn.addEventListener('click', refreshPage)
